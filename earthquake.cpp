9c7b202552619b5212eb912eb97b034fcb590946
Shima <szzzdhrd@memphis.edu>
2015-03-23 16:25:20 -0500

    starting classes

---
+++ earthquake.cpp
@@ -0,0 +1,16 @@
+class earthquake {
+public:
+    set_functions
+    ...
+    get_functions
+    ...
+private:
+    id
+    date
+    time
+    earthquake_name
+    epicenter
+    magnitude type
+    magnitude
+    ...
+};
