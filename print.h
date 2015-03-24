7949f758f3e2b6c64f6cd40d471be019b4cc02f9
Shima <szzzdhrd@memphis.edu>
2015-03-23 20:16:26 -0500

    Class for earthquake, and function print, with some error fixed, still have errors.

---
+++ print.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+        void print_file ( string, ofstream & );
+        void print_file ( int, ofstream & );
+        string uppercase ( string );
