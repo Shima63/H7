cb39cca83ba2c4546fe5cb6e4822f6566585abfd
Shima <szzzdhrd@memphis.edu>
2015-03-26 11:40:20 -0500

    Working fine with earthquake class

---
+++ station.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "print.h"
+
+using namespace std;
+
+class station {
+    
+    public:
+    
+        // Set and Get Functions
+    
+        // Other Functions
+
+    private:
+    
+    string network_code;
+    string station_code;
+    string type_of_band;
+    string type_of_instrument;
+    string orientation;
+
+};
